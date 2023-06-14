89def186568d8197bfacadc738e49c2b256ec93d
Firdaus Ismail <firdausfitri010199@gmail.com>
2023-06-14 23:26:05 +0800

    Fixed search bar can't input space
    Fixed search bar can't input space

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,6 @@ const fetch_promotions = async (skip: number, search: string) => {
 export default function Index() {
   const [promotion_list, set_promotion_list] = useState([]);
   const [promotion_count, set_promotion_count] = useState(0);
-  //const [start, set_start] = useState(1);
   const router = useRouter();
   const page : any = parseInt(router.query.page as string) || 1
   const search : any = router.query.search || ''
@@ -86,7 +85,6 @@ export default function Index() {
             onChange={(event) => {
               router.push(`?page=${1}&search=${event.currentTarget.value}`)
             }}
-            value={search ? search : ""}
             className="h-12 block w-full bg-white dark:bg-gray-900 text-black dark:text-white rounded-md border placeholder-black dark:placeholder-white border-gray-300 dark:border-gray-700 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             placeholder="Search"
           />
